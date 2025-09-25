0c4daca804f89fb03fb4622e1006857ab716dadf
Caleb Pirkle <calebpirkle2@gmail.com>
2025-09-25 01:53:31 -0500

    added weekly transactions fetch, nfl state fetch

--- src/server/api/league.ts
+++ src/server/api/league.ts
@@ -1,20 +1,30 @@
 import type { Request, Response } from "express";
 import { config } from "../config";
-import { NotFoundError } from "../lib/classes/errors";
+import { BadRequestError, NotFoundError } from "../lib/classes/errors";
 
 export async function handlerLeague(_: Request, res: Response) {
 	const leagueId = config.league.id;
 	const league = await getLeague(leagueId),
 		leagueRoster = await getLeagueRosters(leagueId),
 		leagueUsers = await getLeagueUsers(leagueId),
-		thisWeeksLeaguesMatchups = await getThisWeeksLeagueMatchups(leagueId, 3),
-		leagueWinnersBracket = await getLeaguePlayoffBracket(leagueId, "winners_bracket");
+		thisWeeksLeaguesMatchups = await getThisWeeksLeagueMatchups(
+			leagueId,
+			3
+		),
+		leagueWinnersBracket = await getLeaguePlayoffBracket(
+			leagueId,
+			"winners_bracket"
+		),
+		leagueTransactionsWeekThree = await getLeagueTransaction(leagueId, 3),
+		nflState = await getNFLState("nfl");
 	const data = {
 		league,
 		leagueRoster,
 		leagueUsers,
 		thisWeeksLeaguesMatchups,
-		leagueWinnersBracket
+		leagueWinnersBracket,
+		leagueTransactionsWeekThree,
+		nflState
 	};
 
 	res.send(data);
@@ -56,7 +66,10 @@ export async function getLeagueUsers(leagueId: string) {
 	return await leagueUsers.json();
 }
 
-export async function getThisWeeksLeagueMatchups(leagueId: string, week: number) {
+export async function getThisWeeksLeagueMatchups(
+	leagueId: string,
+	week: number
+) {
 	const url = `https://api.sleeper.app/v1/league/${leagueId}/matchups/${week}`;
 	const leagueMatchups = await fetch(url);
 
@@ -69,7 +82,10 @@ export async function getThisWeeksLeagueMatchups(leagueId: string, week: number)
 }
 
 // attempted loses_bracket, currently resulting in a 404, try again in the future
-export async function getLeaguePlayoffBracket(leagueId: string, bracket: 'winners_bracket'|'loses_bracket') {
+export async function getLeaguePlayoffBracket(
+	leagueId: string,
+	bracket: "winners_bracket" | "loses_bracket"
+) {
 	const url = `https://api.sleeper.app/v1/league/${leagueId}/${bracket}`;
 	const playoffBracket = await fetch(url);
 
@@ -79,4 +95,27 @@ export async function getLeaguePlayoffBracket(leagueId: string, bracket: 'winner
 		);
 
 	return await playoffBracket.json();
+}
+
+export async function getLeagueTransaction(leagueId: string, week: number) {
+	const url = `https://api.sleeper.app/v1/league/${leagueId}/transactions/${week}`;
+	const leagueTransactions = await fetch(url);
+	if (!leagueTransactions)
+		throw new NotFoundError(
+			`No transactions found. Either a single transaction wasn't made or perhaps your League ID: ${leagueId} is incorrect.`
+		);
+
+	return await leagueTransactions.json();
+}
+
+export async function getNFLState(sport: "nfl") {
+	const url = `https://api.sleeper.app/v1/state/${sport}`;
+	const nflState = await fetch(url);
+
+	if (!nflState)
+		throw new NotFoundError(
+			`The current state of the NFL according to sleeper was not found. Please check the spelling of the sport: ${sport}`
+		);
+
+	return await nflState.json();
 }
